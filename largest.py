a5fce4e8b82e105eb3f818cb5633c63f5ee4e81f
Ini Ekpenyong Ukut <45959449+ukut@users.noreply.github.com>
2023-03-01 08:15:25 -0800

    Add files via upload

---
+++ largest.py
@@ -0,0 +1,11 @@
+def find_the_largest_number(number_1, number_2):
+    if number_1 > number_2:
+        return number_1
+    elif number_1 < number_2:
+        return number_2
+    else:
+        return number_1
+
+
+# example is given below
+print(find_the_largest_number(5,3))
